aab0e4be993d47394fba33af5e436b4495778774
Elena Koroleva <koroliana@yandex.ru>
2023-02-07 19:25:16 +0700

    hashCode and equals methods

--- src/course1/objects/Main.java
+++ src/course1/objects/Main.java
@@ -6,13 +6,19 @@ public class Main {
         Author stephenFry = new Author("Стивен", "Фрай");
         Book makingHistory = new Book("Как творить историю", stephenFry, 2005);
         Book thereWereNone = new Book("Десять негритят", agathaChristie, 1939);
+        Author author4 = new Author("Стивен", "Фрай");
+        Book book3 = new Book("Как творить историю", author4, 2005);
 
         System.out.println(makingHistory);
         System.out.println(thereWereNone);
-
         makingHistory.setPublicationYear(1996);
-
         System.out.println(makingHistory);
+        System.out.println();
+
+        System.out.println(makingHistory.equals(thereWereNone));
+        System.out.println(makingHistory.hashCode());
+        System.out.println(book3.hashCode());
+        System.out.println(makingHistory.equals(book3));
 
     }
 }
