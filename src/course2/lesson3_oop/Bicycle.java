d05817762c07db8873629e01b9734b36e3cec6e1
Elena Koroleva <koroliana@yandex.ru>
2023-03-07 11:16:49 +0700

    initial commit with given classes

---
+++ src/course2/lesson3_oop/Bicycle.java
@@ -0,0 +1,10 @@
+package course2.lesson3_oop;
+
+public class Bicycle {
+    public String modelName;
+    public int wheelsCount;
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+}
